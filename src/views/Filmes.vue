1eae1837718f855611e85bb0fa14c85f5da45c8a
Jterom <moret4419044@gmail.com>
2021-03-14 22:50:06 +0100

    base

---
+++ src/views/Filmes.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id='app'>
+    <Search/>
+  </div>
+</template>
+
+<script>
+  import Search from '../components/Search'
+  export default {
+    name: 'App',
+    components: {
+      Search
+    }
+  }
+</script>
