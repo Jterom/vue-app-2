2db1196494e3c9aff5f11165f47d0529c69f4483
Jterom <moret4419044@gmail.com>
2021-03-15 14:57:19 +0100

    Detail image

--- src/components/Show.vue
+++ src/components/Show.vue
@@ -2,8 +2,13 @@
     <div class='show'>
         <h1>Detail du films</h1>
         <div>
-            <p>{{this.results.title}}</p>
-            <p>{{this.results.oldtitle}}</p>
+            <img v-bind:src="'http://image.tmdb.org/t/p/w500/' +    results.poster_path" width='100px'>
+            <p>Titre:{{this.results.title}}</p>
+            <p>Titre original:{{this.results.oldtitle}}</p>
+            <p>Genre :</p>
+            <div v-for='result in results.genres' :key='result.id'>
+                <p>{{result.name}}</p>
+            </div>
         </div>
     </div>
 </template>
@@ -16,6 +21,8 @@
             return {
                 query: '',
                 results: {
+                    poster_path:'',
+                    genres:[],
                     title:'',
                     oldtitle:''
 
@@ -25,9 +32,13 @@
         methods: {
             async getResult() {
                 const {data} = await axios.get(`https://api.themoviedb.org/3/movie/${this.$route.params.id}?api_key=1def7fe231547d1124aaf23ed94d0a24&language=en-US`)
-                console.log(data);
-                this.results.title=data.title;
-                this.results.oldtitle=data.original_title;
+                console.log(data)
+                this.results.title = data.title;
+                this.results.oldtitle = data.original_title;
+                this.results.poster_path = data.poster_path
+                for (const dataKey in data.genres) {
+                    this.results.genres.push(data.genres[dataKey]);
+                }
             }
         },
         mounted() {
